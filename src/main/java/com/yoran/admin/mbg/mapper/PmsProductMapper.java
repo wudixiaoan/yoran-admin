520eff5eef1928c6f0bb91307924358f7ba88b0c
panan <pa_more@zhigoumao.cn>
2023-11-18 23:19:13 +0800

    添加数据库配置&mybatis生成器

--- src/main/java/com/yoran/admin/mbg/mapper/PmsProductMapper.java
+++ src/main/java/com/yoran/admin/mbg/mapper/PmsProductMapper.java
@@ -1,4 +1,31 @@
 package com.yoran.admin.mbg.mapper;
 
+import com.yoran.admin.mbg.model.PmsProduct;
+import com.yoran.admin.mbg.model.PmsProductExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface PmsProductMapper {
+    long countByExample(PmsProductExample example);
+
+    int deleteByExample(PmsProductExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PmsProduct row);
+
+    int insertSelective(PmsProduct row);
+
+    List<PmsProduct> selectByExample(PmsProductExample example);
+
+    PmsProduct selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("row") PmsProduct row, @Param("example") PmsProductExample example);
+
+    int updateByExample(@Param("row") PmsProduct row, @Param("example") PmsProductExample example);
+
+    int updateByPrimaryKeySelective(PmsProduct row);
+
+    int updateByPrimaryKey(PmsProduct row);
 }
---
+++ src/main/java/com/yoran/admin/mbg/model/PmsProduct.java
@@ -0,0 +1,127 @@
+package com.yoran.admin.mbg.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+
+public class PmsProduct implements Serializable {
+    private Integer id;
+
+    @ApiModelProperty(value = "商品条码")
+    private String barcode;
+
+    @ApiModelProperty(value = "商品名称")
+    private String name;
+
+    @ApiModelProperty(value = "商品图片")
+    private String pic;
+
+    @ApiModelProperty(value = "商品描述")
+    private String desc;
+
+    @ApiModelProperty(value = "分类ID")
+    private Integer categoryId;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "逻辑删除")
+    private Integer delFlag;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", barcode=").append(barcode);
+        sb.append(", name=").append(name);
+        sb.append(", pic=").append(pic);
+        sb.append(", desc=").append(desc);
+        sb.append(", categoryId=").append(categoryId);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", delFlag=").append(delFlag);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
